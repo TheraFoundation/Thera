aa6ab708a027767f98650f260bd213398076ea78
Derek Santos Sidi <Dereksantossidi@gmail.com>
2020-10-08 08:03:29 -0400

    Reseting Infura and Wallets
    Using ENvariables to hide credentials

--- truffle-config.js
+++ truffle-config.js
@@ -20,20 +20,19 @@ module.exports = {
     mainnet: {
       // must be a thunk, otherwise truffle commands may hang in CI
       provider: () =>
-        new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/fd39f71cc4d14deb8e341de44eb13245",
-            0, 1, true, "m/44'/1'/0'/0/"
+        new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY,
         ),
       network_id: '1',
     },
     mainnetFork: {
       // must be a thunk, otherwise truffle commands may hang in CI
-      url: "https://sandbox.truffleteams.com/c4567ffb-8aa7-4c08-8f52-7f29fdd57b78",
+      url: "https://sandbox.truffleteams.com/eb7fdd7f-6a9c-42a0-8068-b7a3847024e9",
       network_id: '1',
     },
     ropsten: {
       // must be a thunk, otherwise truffle commands may hang in CI
       provider: () =>
-        new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/fd39f71cc4d14deb8e341de44eb13245",
+        new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY,
             0, 1, true, "m/44'/60'/0'/0/"
         ),
       network_id: '3',
